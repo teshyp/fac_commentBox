241ea663aeb3bdd3a3c4244be23b3e2a7feb157b
Tesh Patel <tesh.p@hotmail.co.uk>
2022-10-07 23:27:22 +0100

    Working on adding user response

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const formInput = document.getElementById("comment");
-
 const submitBtn = document.getElementById("submitBtn");
-
 const commentInput = document.getElementById("comment");
-
 const charCounter = document.getElementById("charCounter");
+const userName = document.getElementById("fullName");
+const userEmail = document.getElementById("email");
+
+const parentRepliesDiv = document.querySelector("#comment-chain");
+
+console.log(typeof parentRepliesDiv);
 
 commentInput.addEventListener("keydown", keydowntest);
 
@@ -13,11 +16,19 @@ function keydowntest() {
   if (commentInput.value.length === 140) charCounter.style.color = "red";
 }
 
-submitBtn.addEventListener("click", test);
+submitBtn.addEventListener("click", appendReplies);
 
-function test() {
-  console.log(commentInput.value.length);
-  if (commentInput.value.length > 140) {
-    console.log("Over");
+function appendReplies() {
+  function createUserResponse() {
+    const displayUserName = document.createElement("h3");
+    displayUserName.innerText = userName.value;
+    return displayUserName;
   }
+  // create h2 for name
+  // creae box comment
+  // append to replies container
+
+  parentRepliesDiv.appendChild(createUserResponse());
+
+  console.log("Working");
 }
